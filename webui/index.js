73e89ddcd4a9c9a39758876c5ff1119e1093174b
François <juef17@users.noreply.github.com>
2024-05-25 08:25:47 -0400

    Initial commits for this branch

--- webui/index.js
+++ webui/index.js
@@ -84,7 +84,6 @@ class Rom extends Uint8Array {
     randomize(seed, flags, sprite) {
         console.log("Randomizing...");
         FS.writeFile('/'+this.name, this);
-
         this.seed = seed;
         this.flags = flags;
         let checksum = Module.ccall('dwr_randomize', 'BigInt',
@@ -118,145 +117,212 @@ class Rom extends Uint8Array {
  */
 function setup_ui() {
     if (!localStorage.flags)
-        localStorage.flags = 'IVIAAVCAKACAAAAAAAAAAEAA'
+        localStorage.flags = 'IVIAAVCAKACAAAAAAAAAAEAAAAIAAAAA'
     if (!localStorage.retainFlags)
-        localStorage.retainFlags = 'AAAAAAAAAAAAAAAAAAAAAAAA'
-    ui = new Interface(15);
-    ui.addTab('Gameplay');
-    ui.addTab('Features');
-    ui.addTab('Monsters');
-    ui.addTab('Shortcuts');
-    ui.addTab('Challenge');
-    ui.addTab('Cosmetic');
+        localStorage.retainFlags = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    ui = new Interface(20);
+    ui.addTab('Gamepl.');
+    ui.addTab('Feat.');
+    ui.addTab('Monst.');
+    ui.addTab('Shortc.');
+    ui.addTab('Chall.');
+    ui.addTab('Cosm.');
+    ui.addTab('New 1');
+    ui.addTab('New 2');
     ui.addSummaryTab('Summary');
-    ui.setActiveTab('Gameplay');
+    ui.setActiveTab('Gamepl.');
 
-    ui.addTriOption('Gameplay',  0,  0, 6, 'Shuffle Chests & Searches',
+    ui.addTriOption('Gamepl.',  0,  0, 6, 'Shuffle Chests & Searches',
         'The items in chests and search locations will be randomized.');
-    ui.addTriOption('Gameplay',  2,  0, 4, 'Random Chest Locations',
+    ui.addTriOption('Gamepl.',  2,  0, 4, 'Random Chest Locations',
         'Chests will be at a random set of predetermined locations.');
-    ui.addTriOption('Gameplay',  4,  0, 2, 'Random Growth',
+    ui.addTriOption('Gamepl.',  4,  0, 2, 'Random Growth',
         'Player statistical growth at each level will be randomized.');
-    ui.addTriOption('Gameplay',  6,  0, 0, 'Random Map',
+    ui.addTriOption('Gamepl.',  6,  0, 0, 'Random Map',
         'The overworld map will be randomly generated.');
-    ui.addTriOption('Gameplay',  8,  1, 6, 'Random Spell Learning',
+    ui.addTriOption('Gamepl.',  8,  1, 6, 'Random Spell Learning',
         'The order and level you learn spells will be random.');
-    ui.addTriOption('Gameplay',  1,  1, 4, 'Random Weapon Shops',
+    ui.addTriOption('Gamepl.',  1,  1, 4, 'Random Weapon Shops',
         'The weapons available in each shop will be randomized.');
-    ui.addTriOption('Gameplay',  3,  1, 2, 'Random Weapon Prices',
+    ui.addTriOption('Gamepl.',  3,  1, 2, 'Random Weapon Prices',
         'Pretty self-explanatory.');
-    ui.addTriOption('Gameplay',  5,  1, 0, 'Random XP Requirements',
+    ui.addTriOption('Gamepl.',  5,  1, 0, 'Random XP Requirements',
         'Experience requirements for each level will be randomized.');
-    ui.addTriOption('Gameplay',  7,  2, 6, 'Heal/Hurt Before "More"',
+    ui.addTriOption('Gamepl.',  7,  2, 6, 'Heal/Hurt Before "More"',
         'HEAL must come before HEALMORE; HURT before HURTMORE.');
-    ui.addTriOption('Gameplay', 10,  2, 2, 'Stair Shuffle',
+    ui.addTriOption('Gamepl.', 10,  2, 2, 'Stair Shuffle',
         'Where stairs take you inside dungeons will be randomized.');
 
-    ui.addTriOption('Features',  0,  3, 6, 'Enable Menu Wrapping',
+    ui.addTriOption('Feat.',  0,  3, 6, 'Enable Menu Wrapping',
         'This enables cursor wrapping in menus.');
-    ui.addTriOption('Features',  2,  3, 4, 'Enable Death Necklace',
+    ui.addTriOption('Feat.',  2,  3, 4, 'Enable Death Necklace',
         'Equipping the death necklace will cause +10AP and -25% HP.');
-    ui.addTriOption('Features',  4,  3, 2, 'Enable Torches In Battle',
+    ui.addTriOption('Feat.',  4,  3, 2, 'Enable Torches In Battle',
         'Torches and Fairy water can be thrown at monsters.');
-    ui.addTriOption('Features',  1,  4, 6, 'Repel in Dungeons',
+    ui.addTriOption('Feat.',  1,  4, 6, 'Repel in Dungeons',
         'Enables REPEL to work in dungeons');
-    ui.addTriOption('Features',  3,  4, 4, 'Permanent Repel',
+    ui.addTriOption('Feat.',  3,  4, 4, 'Permanent Repel',
         'REPEL will always be active.');
-    ui.addTriOption('Features',  5,  4, 2, 'Permanent Torch',
+    ui.addTriOption('Feat.',  5,  4, 2, 'Permanent Torch',
         'At least a 3x3 area will always be lit in dungeons.');
-    ui.addTriOption('Features',  6,  4, 0, 'Alternate Running Algorithm',
+    ui.addTriOption('Feat.',  6,  4, 0, 'Alternate Running Algorithm',
         'The run blocking multiplier will depend on where you are.');
-    ui.addTriOption('Monsters',  0,  5, 6, 'Random Monster Abilities',
+    ui.addTriOption('Monst.',  0,  5, 6, 'Random Monster Abilities',
         'Monster spells and abilities will be randomized.');
-    ui.addTriOption('Monsters',  2,  5, 4, 'Random Monster Zones',
+    ui.addTriOption('Monst.',  2,  5, 4, 'Random Monster Zones',
         'Monster encounters in each zone will be randomized.');
-    ui.addTriOption('Monsters',  4,  5, 2, 'Random Monster Stats',
+    ui.addTriOption('Monst.',  4,  5, 2, 'Random Monster Stats',
         'Monster strength, agility, and HP will be randomized.');
-    ui.addTriOption('Monsters',  6,  5, 0, 'Random Monster XP & Gold',
+    ui.addTriOption('Monst.',  6,  5, 0, 'Random Monster XP & Gold',
         'The XP and GOLD gained from monsters will be randomized.');
-    ui.addTriOption('Monsters',  8,  6, 6, 'Make Random Stats Consistent',
+    ui.addTriOption('Monst.',  8,  6, 6, 'Make Random Stats Consistent',
         'This makes the random stats, XP, and GOLD consistent with one another.');
-    ui.addTriOption('Monsters',  1,  6, 4, 'Scared Metal Slimes',
+    ui.addTriOption('Monst.',  1,  6, 4, 'Scared Metal Slimes',
         'Metal Slimes will always have a chance to run.');
-    ui.addTriOption('Monsters',  3,  6, 2, 'Scaled Metal Slime XP',
+    ui.addTriOption('Monst.',  3,  6, 2, 'Scaled Metal Slime XP',
         'Metal Slime XP will depend on your current level.');
 
-    ui.addTriOption('Shortcuts',  0,  7, 6, 'Fast Text',
+    ui.addTriOption('Shortc.',  0,  7, 6, 'Fast Text',
         'All text will progress much faster.');
-    ui.addTriOption('Shortcuts',  2,  7, 4, 'Speed Hacks',
+    ui.addTriOption('Shortc.',  2,  7, 4, 'Speed Hacks',
         'Various aspects of the game will be much faster.');
-    ui.addTriOption('Shortcuts',  4,  7, 2, 'Open Charlock',
+    ui.addTriOption('Shortc.',  4,  7, 2, 'Open Charlock',
         'No need to create a bridge to enter Charlock Castle.');
-    ui.addTriOption('Shortcuts',  6,  7, 0, 'Short Charlock',
+    ui.addTriOption('Shortc.',  6,  7, 0, 'Short Charlock',
         'Charlock Dungeon will be much shorter.');
-    ui.addTriOption('Shortcuts',  8,  8, 6, "Don't Require Magic Keys",
+    ui.addTriOption('Shortc.',  8,  8, 6, "Don't Require Magic Keys",
         'All of the doors are unlocked. Just open them.');
-    ui.addTriOption('Shortcuts', 10, 13, 2, "Summer Sale",
+    ui.addTriOption('Shortc.', 10, 13, 2, "Summer Sale",
         'All weapons and armor 35-65% off!');
-    ui.addTriOption('Shortcuts',  1,  8, 2, 'Cursed Princess',
+    ui.addTriOption('Shortc.',  1,  8, 2, 'Cursed Princess',
         'Get Gwaelin to take a Cursed Belt when you return her to win.');
-    ui.addTriOption('Shortcuts',  3,  8, 0, "Three's Company",
+    ui.addTriOption('Shortc.',  3,  8, 0, "Three's Company",
         'Bring Gwaelin to the Dragonlord and accept his offer to win.');
     // leveling speed
-    ui.addDropDown ('Shortcuts',  7, 13, 4, 'Leveling Speed', {
+    ui.addDropDown ('Shortc.',  7, 13, 4, 'Leveling Speed', {
         'Normal' : 0,
         'Fast' : 1,
         'Very Fast': 2
     });
-    ui.addDropDown ('Shortcuts',  9, 13, 6, 'Random Map Size', {
+    ui.addDropDown ('Shortc.',  9, 13, 6, 'Random Map Size', {
         'Normal' : 0,
         'Small' : 1,
         'Very Small': 2,
         'Random': 3
     });
 
-    ui.addTriOption('Challenge',  0,  9, 6, 'No Hurtmore',
+    ui.addTriOption('Chall.',  0,  9, 6, 'No Hurtmore',
         'You will never learn HURTMORE. Monsters can still have it.');
-    ui.addTriOption('Challenge',  2,  9, 4, 'No Numbers',
+    ui.addTriOption('Chall.',  2,  9, 4, 'No Numbers',
         'No numbers will be visible until the Dragonlord fight.');
-    ui.addTriOption('Challenge',  4,  9, 2, 'Invisible Hero',
+    ui.addTriOption('Chall.',  4,  9, 2, 'Invisible Hero',
         'Your sprite will be invisible.');
-    ui.addTriOption('Challenge',  6,  9, 0, 'Invisible NPCs',
+    ui.addTriOption('Chall.',  6,  9, 0, 'Invisible NPCs',
         'All NPCs will be invisible.');
-    ui.addTriOption('Challenge',  1, 13, 0, 'Treasure Guards',
+    ui.addTriOption('Chall.',  1, 13, 0, 'Treasure Guards',
         'Important items will have a mid-level monster guarding them.');
-    ui.addTriOption('Challenge',  3, 10, 0, 'Big Swamp',
+    ui.addTriOption('Chall.',  3, 10, 0, 'Big Swamp',
         'Approximately 60% of the overworld will be poisonous swamp.');
-    ui.addTriOption('Challenge',  5, 10, 2, 'Randomly Rotate/Mirror Dungeons',
+    ui.addTriOption('Chall.',  5, 10, 2, 'Randomly Rotate/Mirror Dungeons',
         'All dungeons will be rotated at random angles and/or mirrored.');
-    ui.addTriOption('Challenge',  7, 10, 4, "No Armor in Charlock",
+    ui.addTriOption('Chall.',  7, 10, 4, "No Armor in Charlock",
         "Prevent Erdrick's Armor from being in a chest in Charlock Castle.");
-    ui.addTriOption('Challenge',  9, 10, 6, "Easy Charlock",
+    ui.addTriOption('Chall.',  9, 10, 6, "Easy Charlock",
         "Make it slightly easier to run from high level monsters.");
 
-    ui.addTriOption('Cosmetic',  4, 11, 6, 'Modern Spell Names',
+    ui.addTriOption('Cosm.',  4, 11, 6, 'Modern Spell Names',
         'Use spell names from more recent DQ releases.');
-    ui.addTriOption('Cosmetic',  7, 11, 4, 'Noir Mode',
+    ui.addTriOption('Cosm.',  7, 11, 4, 'Noir Mode',
         "It's all black and white baby!");
-    ui.addOption   ('Cosmetic',  0, 14, 7, 'Shuffle Music',
+    ui.addOption   ('Cosm.',  0, 14, 7, 'Shuffle Music',
         'Music in each area will be randomized.');
-    ui.addOption   ('Cosmetic',  2, 14, 6, 'Disable Music',
+    ui.addOption   ('Cosm.',  2, 14, 6, 'Disable Music',
         'This disables the game music in most situations.');
-    ui.addOption   ('Cosmetic',  9, 14, 5, 'Disable Spell Flashing',
+    ui.addOption   ('Cosm.',  9, 14, 5, 'Disable Spell Flashing',
         'Prevents the screen from flashing when you cast spells.', true);
-    ui.addOption   ('Cosmetic',   6, 14, 4, 'Show Death Counter',
+    ui.addOption   ('Cosm.',   6, 14, 4, 'Show Death Counter',
         'The stats window will also have a death counter.');
-    ui.addOption   ('Cosmetic',   3, 14, 3, 'Allow Custom Spell Names',
+    ui.addOption   ('Cosm.',   3, 14, 3, 'Allow Custom Spell Names',
         'Allow spell names to be changed based on the chosen sprite.', true);
-    ui.addOption   ('Cosmetic',   5, 14, 2, 'Skip Original Credits',
+    ui.addOption   ('Cosm.',   5, 14, 2, 'Skip Original Credits',
         'Skip the original credits and go straight to stat scroll.', true);
+    ui.addOption   ('Cosm.',   5, 14, 2, 'Skip Original Credits',
+        'Skip the original credits and go straight to stat scroll.', true);
+
+    ui.addTriOption('New 1',   0, 16, 2, 'Disguised Dragonlord',
+        'The Dragonlord is now roaming Alefgard as a random NPC. The original Dragonlord location gives an hint to his whereabouts.');
+    ui.addTriOption('New 1',   2, 16, 4, 'Randomize Flute Music',
+        'Change the Fairy Flute sound to a new random tune.');
+    ui.addTriOption('New 1',   4, 16, 6, 'Vendor Shuffle',
+        'Weapon, fairy water, item, radishes and key vendors (except the Rimuldar key vendor) are shuffled.');
+    ui.addTriOption('New 1',   6, 15, 6, 'Charlock Inn',
+        'Make the final dive easier by having a comfy bed and breakfast at the Dragonlord\'s.');
+    ui.addTriOption('New 1',   8, 15, 4, 'Only Healmore',
+        'Never learn any spell other than this. Like the No Hurtmore flag, only works with Randomized Spells on.');
+    ui.addTriOption('New 1',  10, 16, 0, 'Magic Herbs',
+        'Make herbs refill MP rather than HP.');
+    ui.addDropDown ('New 1',   1, 15, 2, 'Inn Prices', {
+        'Vanilla' : 0,
+        'Shuffled' : 1,
+        'Random 1G-255G' : 2,
+        'One of the above': 3
+    });
+    ui.addDropDown ('New 1',   3, 15, 0, 'Key Prices', {
+        'Vanilla' : 0,
+        'Shuffled' : 1,
+        'Random 1G-255G' : 2,
+        'One of the above': 3
+    });
+    ui.addTriOption('New 1',   5, 17, 2, 'DL1 Crits',
+        'Allow excellent moves against the Dragonlord\'s 1st form.');
+    ui.addTriOption('New 1',   7, 17, 0, 'DL2 Crits',
+        'Allow excellent moves against the Dragonlord\'s 2nd form.');
+    ui.addDropDown ('New 1',   9, 17, 4, 'Crit Chance', {
+        'Vanilla (1/32)' : 0,
+        'Double (1/16)' : 1,
+        'Always (100%)' : 2,
+        'Random (1-32)/32' : 3,
+        'One of the above': 4
+    });
+    ui.addDropDown ('New 1',  11, 18, 5, 'Bonk Dmg', {
+        'None' : 0,
+        '1 HP' : 1,
+        '2 HP' : 2,
+        '20 HP' : 3,
+        'Instakill' : 4,
+        'Random (0-80)': 5,
+        'One of the above': 6
+    });
+    ui.addTriOption('New 2',  0, 18, 3, 'Discardable Flute',
+        'Make it possible to drop the flute on finding a new item with a full inventory.');
+    ui.addDropDown ('New 2',  1, 18, 1, 'Flute is for', {
+        'Golem' : 0,
+        'All but DL2' : 1,
+        'All' : 2,
+        'One of the above': 3
+    });
+    ui.addOption   ('New 2',  2, 17, 7, 'Disable Red Flashes',
+        'Prevents the screen from flashing when walking on damage tiles.');
+    ui.addOption   ('New 2',  3, 18, 0, 'Level 1 Radiant',
+        'If spells are randomized, makes sure the hero always knows Radiant.');
+    ui.addOption   ('New 2',  4, 19, 7, 'Return Escapes',
+        'Return can be used in battle for a guaranteed escape.');
+    ui.addOption   ('New 2',  5, 19, 6, 'Return to Town',
+        'Wings and Return send you to the last place you saved or used an inn at.');
+    ui.addOption   ('New 2',  6, 19, 5, 'Warp Whistle',
+        'The Fairy Flute will work as a warp whistle outside of battle, cycling between places you saved or used an inn at.');
 
     // player sprite
     let spriteBox;
     if (!isElectron()) {
-        spriteBox = ui.addTextBox('Cosmetic', 1, 'Player Sprite');
+        spriteBox = ui.addTextBox('Cosm.', 1, 'Player Sprite');
         spriteBox.setAttribute('list', 'sprites');
         spriteBox.value = localStorage.getItem('sprite') || 'Random';
         spriteBox.addEventListener('focus', function(event) {
             this.value = '';
         });
     } else {
-        spriteBox = ui.addDropDown('Cosmetic', 1, null, null, 
+        spriteBox = ui.addDropDown('Cosm.', 1, null, null,
             'Player Sprite', null, true);
         spriteBox.style.marginRight = '0.2em';
         spriteBox.style.width = '190px';
